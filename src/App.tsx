eb02ae6a674ef38ebdda821772524dcc07550407
juan <juan_faria@tecsinapse.com.br>
2024-06-19 18:17:32 -0300

    Projeto Criado

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav className='p-5'>
+          <p className=''>
+              Home
+          </p>
+          Forms
+      </nav>
+    </>
+  )
+}
+
+export default App
