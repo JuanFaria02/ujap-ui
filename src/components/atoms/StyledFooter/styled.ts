8ac160d5f1b35438b15151bea1a77ef5e9a54b88
juan <juan_faria@tecsinapse.com.br>
2024-07-22 23:27:37 -0300

    Mudança no componente Link

--- src/components/atoms/StyledFooter/styled.ts
+++ src/components/atoms/StyledFooter/styled.ts
@@ -1,6 +1,6 @@
 import { styled, Theme } from "@mui/material";
 
-const StyledFooter = styled('footer')(({ theme }: { theme: Themee }) => ({
+const StyledFooter = styled('footer')(({ theme }: { theme: Theme }) => ({
     backgroundColor: theme.palette.info.main,
   }));
   
--- src/components/structure/Footer/Footer.tsx
+++ src/components/structure/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { FooterContainer } from "../../atoms/StyledFooter";
-import { Box, Container } from "@mui/material";
 import { FacebookIcon, InstagramIcon } from "../../icons";
 import { getUrlFacebookUjap, getUrlInstagramUjap } from "./functions";
 import { Link } from "react-router-dom";
@@ -13,12 +12,12 @@ const Footer = () => {
   return (
     <FooterContainer className='h-28 absolute w-full bottom-0 content-center'>
       <div className='relative inline ml-[48%]'>
-            <Link target='_blank' to={getUrlFacebookUjap()}>
+            <a target='_blank' href={getUrlFacebookUjap()}>
                   <FacebookIcon color='primary'/>
-            </Link>
-            <Link className='ml-2' target='_blank' to={getUrlInstagramUjap()}>
+            </a>
+            <a className='ml-2' target='_blank' href={getUrlInstagramUjap()}>
                   <InstagramIcon color='primary'/>
-            </Link>
+            </a>
       </div>
     </FooterContainer>
   );
